025f6de54b467522ba9fec095952d91e2b86b2b2
gurpreet203181 <45843946+gurpreet203181@users.noreply.github.com>
2021-04-30 08:43:57 +0200

    Add project files.

---
+++ NarcisAsyncAwait/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NarcisAsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
